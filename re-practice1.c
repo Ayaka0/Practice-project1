8cbc81a341be482d28f012ea917f84f98a99c799
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-06-12 21:09:33 +0900

    Add files via upload

--- re-practice1.c
+++ re-practice1.c
@@ -3,28 +3,19 @@
 int main(void)
 {
 
-	int a, b;
-	int lower, upper;
+	int no;
 
-	printf("®”a;"); scanf("%d", &a);
-	printf("®”b;"); scanf("%d", &b);
+	printf("³‚Ì®”‚ð“ü—ÍG");
+	scanf("%d", &no);
 
-	if (a > b) {
-		lower = b;
-		upper = a;
-	} else {
-		lower = a;
-		upper = b;
+	if (no < 0)
+		return (0);
+
+	while (no >= 0) {
+		printf("%d", no);
+		no--;
 	}
-	
-	int sum = 0;
-	int no = lower;
-	do {
-		sum += no;
-		no++;
-	} while (no <= upper);
-
-	printf("%dˆÈã%dˆÈ‰º‚Ì‘S®”‚Ì˜a‚Í%d‚Å‚·B\n", lower, upper, sum);
+	putchar('\n');
 
 	return 0;
 
