09a85523806fbbe0dd6be422d95e930ca118815d
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-06-12 20:34:10 +0900

    Add files via upload

--- re-practice1.c
+++ re-practice1.c
@@ -2,21 +2,24 @@
 
 int main(void)
 {
-	puts("正の整数値を加算します（終了は-9999）");
 
-	int sum = 0;
-	while (1) {
+	int retry;
+
+	do {
 		int no;
-
-		printf("整数値");
+		printf("整数を入力せよ");
 		scanf("%d", &no);
-		if (no == -9999)
-			break;
-		else if (no <= 0)
-			continue;
-		sum += no;
-	}
-	printf("正の整数の合計は%dです。", sum);
+
+		if (no == 0)
+			puts("その数は０です");
+		else if (no > 0)
+			puts("その数は正です");
+		else
+			puts("その数は負です");
+
+		printf("again? yes: 0 / No: 1");
+		scanf("%d", &retry);
+	} while (retry == 0);
 
 	return 0;
 
