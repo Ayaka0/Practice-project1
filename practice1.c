1f8eb329191e2ebe3e7db203456ec5790a4b60a3
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-06-28 21:45:49 +0900

    Add files via upload

--- practice1.c
+++ practice1.c
@@ -3,15 +3,16 @@
 int main(void)
 {
 
-	int x, y;
-	printf("2つの整数を入力せよ。\n");
+	int no;
 
-	printf("整数x："); scanf("%d", &x);
-	printf("整数y："); scanf("%d", &y);
+	printf("整数を入力せよ。\n"); scanf("%d", &no);
 
 
-	printf("xの値はyの%d%%です。", 100 * x / y);
-
+	printf("7を加えると%dです。\n", no + 7);
+	printf("7を減じると%dです。\n", no - 7);
+	printf("7を乗じると%dです。\n", no * 7);
+	printf("7を除した商は%dです。\n", no / 7);
+	printf("7を除した余剰%dです。\n", no % 7);
 
 	return 0;
 }
