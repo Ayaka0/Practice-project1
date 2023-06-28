abe619a3b58c59df192b620d94841446d489a794
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-06-28 22:52:15 +0900

    Add files via upload

--- practice1.c
+++ practice1.c
@@ -2,14 +2,30 @@
 
 int main(void)
 {
+	int n1, n2;
+	double d1, d2;
 
-	double no;
+	printf("整数と実数を２つずつ入力せよ\n");
+	printf("整数n1：");  scanf("%d", &n1);
+	printf("整数n2：");  scanf("%d", &n2);
+	printf("実数d1：");  scanf("%lf", &d1);
+	printf("実数d2：");  scanf("%lf", &d2);
 
-	printf("半径："); scanf("%lf", &no);
+	printf("5 * 2 = %d\n", 5 * 2);
+	printf("5 / 2 = %d\n", 5 / 2);
+	printf("5 * n1 = %d\n", 5 * n1);
+	printf("5 / n1 = %d\n", 5 / n2);
+	printf("n1 * n2 = %d\n", n1 * n2);
+	printf("n1 / n2 = %d\n", n1 / n2);
 
-	double half = no * no * 3.14;
-
-	printf("円の面積は%fです。\n",half);
+	printf("5 * 2.5 = %f\n", 5 * 2.5);
+	printf("5 / 2.5 = %f\n", 5 / 2.5);
+	printf("5 * d1 = %f\n", 5 * d1);
+	printf("5 / d1 = %f\n", 5 / d1);
+	printf("5 * d2 = %f\n", 5 * d2);
+	printf("5 / d2 = %f\n", 5 / d2);
+	printf("n1 * d2 = %f\n", n1 * d2);
+	printf("n1 / d2 = %f\n", n1 / d2);
 
 	return 0;
 }
