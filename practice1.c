c1a649dcac9e27e950f3d96b525f1eff14abd56d
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-06-29 21:42:24 +0900

    Add files via upload

--- practice1.c
+++ practice1.c
@@ -2,16 +2,16 @@
 
 int main(void)
 {
-	int n1, n2;
+	int n1, n2, n3;
 
-	puts("2つの整数を入力せよ");
+	puts("3つの整数を入力せよ");
 	printf("整数１："); scanf("%d", &n1);
 	printf("整数２："); scanf("%d", &n2);
+	printf("整数３："); scanf("%d", &n3);
 
-	if (n1 > n2)
-		printf("それらの差は%dです。\n", n1 - n2);
-	else
-		printf("それらの差は%dです。\n", n2- n1);
+	int min = n1 < n2 ? n1 < n3 ? n1 : n3 : n2 < n3 ? n2 : n3;
+	printf("最小値は%dです。\n", min);
+	
 
 	return 0;
 }
