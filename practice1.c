58e64be252da24ef92c29ac6efa8e3a444da0bdb
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-06-29 22:04:47 +0900

    Add files via upload

--- practice1.c
+++ practice1.c
@@ -2,19 +2,16 @@
 
 int main(void)
 {
-	int n1, n2, n3;
+	int n1, n2;
 
 	puts("3つの整数を入力せよ");
 	printf("整数１："); scanf("%d", &n1);
 	printf("整数２："); scanf("%d", &n2);
-	printf("整数３："); scanf("%d", &n3);
 
-	if (n1 == n2 && n2 == n3)
-		printf("3つの値が等しい");
-	else if (n1 == n2 || n2 == n3 || n1 == n3)
-		printf("2つの値が等しい");
+	if (n1 - n2 >10 || n2 - n1 > 10)
+		printf("それらの差は11以上です。\n");
 	else 
-		printf("3つとも値は異なります");
+		printf("それらの差は10以下です\n");
 	
 
 	return 0;
