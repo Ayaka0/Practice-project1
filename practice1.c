da95be6ef1cb86d9db00206e0ac1c47e292fb0b6
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-06-29 21:29:51 +0900

    Add files via upload

--- practice1.c
+++ practice1.c
@@ -2,23 +2,20 @@
 
 int main(void)
 {
-	int a,b;
+	int n1, n2, n3,n4;
 
-	printf("２つの整数を入力せよ；");
-	printf("整数A：");  scanf("%d", &a);
-	printf("整数B：");  scanf("%d", &b);
+	puts("３つの整数を入力せよ");
+	printf("整数１："); scanf("%d", &n1);
+	printf("整数２："); scanf("%d", &n2);
+	printf("整数３："); scanf("%d", &n3);
+	printf("整数４："); scanf("%d", &n4);
 
+	int max = n1;
+	if (n2 > max) max = n2;
+	if (n3 > max) max = n3;
+	if (n4 > max) max = n4;
 
-	puts("等価式の値");
-		printf("a==bの値：%d\n", a==b);
-		printf("a!=bの値：%d\n", a != b);
-	
-	puts("関係式の値");
-		printf("a < bの値：%d\n", a < b);
-		printf("a <= bの値：%d\n", a <= b);
-		printf("a > bの値：%d\n", a > b);
-		printf("a >= bの値：%d\n", a >= b);
-	
+	printf("最小値は%dです\n", max);
 
 	return 0;
 }
