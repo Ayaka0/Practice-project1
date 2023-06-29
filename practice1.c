1952ee5c14637b82fb646522ebefa5812666d068
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-06-29 22:23:42 +0900

    Add files via upload

--- practice1.c
+++ practice1.c
@@ -2,16 +2,21 @@
 
 int main(void)
 {
-	int n;
+	int no;
 
-	printf("整数を入力せよ");
-	scanf("%d", &n);
+	printf("点数：");
+	scanf("%d", &no);
 
-	switch (n % 2) {
-	case 0: puts("その数は偶数です。"); break;
-	case 1: puts("その数は奇数です。"); break;
-	}
-	
+	if (no > 100|| no < 0)
+		printf("不正な点数");
+	else if (no < 60)
+		printf("不可");
+	else if (no < 70)
+		printf("可");
+	else if (no < 80)
+		printf("良");
+	else
+		printf("優");
 
 	return 0;
 }
