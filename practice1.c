81f7fdcac5f48e30f467738f511e302061249e81
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-07-13 21:39:57 +0900

    Add files via upload

--- practice1.c
+++ practice1.c
@@ -3,15 +3,16 @@
 int main(void) {
 
 	int no;
-
-	printf("ê≥ÇÃêÆêî");
-	scanf("%d",&no);
-
 	
-	while (no-- > 0) {
-		putchar('*');
-		putchar('\n');
-	}
+		printf("ê≥ÇÃêÆêîÅF");
+		scanf("%d", &no);
 
+		printf("%dÇãtÇ©ÇÁì«ÇﬁÇ∆", no);
+		while (no > 0) {
+			printf("%d", no % 10);
+			no /= 10;
+		}
+		puts("Ç≈Ç∑ÅB\n");
+		
 	return 0;
 }
