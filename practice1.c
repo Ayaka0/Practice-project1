3c696c9f4b401b5e455b7d44e1aa2cfdd53ab6a2
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-07-17 19:24:19 +0900

    Add files via upload

--- practice1.c
+++ practice1.c
@@ -1,19 +1,28 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(void) {
 
-	int no;
-	
-		printf("正の整数：");
-		scanf("%d", &no);
+unsigned long getAreaTriangle(unsigned long bottom, unsigned long height)
+{
+	unsigned long area;
 
-		printf("%dは", no);
-		int i = 0;
-		while (no > 0) {
-			no /= 10;
-			i++;
-		}
-		printf("%d桁です。", i);
-		
+	//面積を計算
+	area = bottom * height / 2;
+
+	//面積を戻り値で返却
+	return area;
+}
+
+
+
+int main(void)
+{
+	unsigned long bottom = 15;
+	unsigned long height = 20;
+	unsigned long area;
+
+	area = getAreaTriangle(bottom, height);
+
+	printf("底辺：%d 高さ：%d 面積：%d",bottom, height, area);
+
 	return 0;
 }
