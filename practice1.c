5408a8637721c6223a06570f5f73b05c6c45c29a
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-07-23 20:08:00 +0900

    Add files via upload

--- practice1.c
+++ practice1.c
@@ -2,27 +2,15 @@
 
 int main(void)
 {
-	int no1, no2, height, width;
-	printf("横長の長方形を作ります。\n");
-	printf("一辺（その１）："); scanf("%d", &no1);
-	printf("一辺（その２）："); scanf("%d", &no2);
+	int len;
+
+	puts("短辺：");
+	scanf("%d", &len);
 
 	int i, j;
-
-	if (no1 < no2)
+	for (i = len; i >= 1; i--)
 	{
-		height = no1;
-		width = no2;
-	}
-	else
-	{
-		height = no2;
-		width = no1;
-	}
-
-	for (i = 1; i <= height; i++)
-	{
-		for (j = 1; j <= width; j++)
+		for (j = 1; j <= i; j++)
 			putchar('*');
 		putchar('\n');
 	}
