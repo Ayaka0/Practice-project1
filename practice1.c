40e572906e674f90f4fe66a5b2b9bae1cfb1b3fa
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-07-25 21:16:54 +0900

    Add files via upload

--- practice1.c
+++ practice1.c
@@ -2,22 +2,12 @@
 
 int main(void)
 {
-	int len;
+	int a[5] = {5,4,3,2,1};
 
-	printf("ピラミッド：");
-	scanf("%d", &len);
+	int i;
 
-	int i, j;
-
-	for (i = 1; i <= len; i++)
-	{
-		for (j = 1; j <= i - 1; j++)
-			putchar(' ');
-		for (j = 1; j <= 2 * (len - i) + 1; j++)
-			printf("%d", i % 10);
-		putchar('\n');
-	}
-
+	for (i = 0; i < 5; i++)
+		printf("a[%d] = %d\n", i, a[i]);
 
 	return 0;
 
