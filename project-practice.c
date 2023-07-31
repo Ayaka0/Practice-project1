2cf09a1aa6bad6ba3b336e2a14f9b41653256fb3
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-07-31 21:47:02 +0900

    Add files via upload

--- project-practice.c
+++ project-practice.c
@@ -6,12 +6,22 @@ int main(void)
 	int b[5]; //コピー先配列
 
 	int i;
-	for (i = 0; i < 5; i++)
+	for (i = 0; i < 5; i++) //要素に値を読み込む
 	{
 		printf("a[%d] : ", i);
 		scanf("%d", &a[i]);
 	}
 
+	/*配列aも並びを逆にする*/
+	for (i = 0; i < 2; i++)
+	{
+		int t = a[i];
+		a[i] = a[4 - i];
+		a[4 - i] = t;
+	}
+
+
+	/*配列aを配列bにコピーする*/
 	for (i = 0; i < 5; i++)
 		b[i] = a[i];
 
