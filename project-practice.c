3387346ece426a722bf5fa24698927017fcf4610
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-07-31 21:31:55 +0900

    Add files via upload

---
+++ project-practice.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int a[5]; //コピー元配列
+	int b[5]; //コピー先配列
+
+	int i;
+	for (i = 0; i < 5; i++)
+	{
+		printf("a[%d] : ", i);
+		scanf("%d", &a[i]);
+	}
+
+	for (i = 0; i < 5; i++)
+		b[i] = a[i];
+
+	puts("   a    b");
+	puts("---------");
+	for (i = 0; i < 5; i++)
+		printf("%4d%4d\n", a[i], b[i]);
+
+
+	return 0;
+}
