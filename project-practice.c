8fe562d898f0093646001fe4811218b5f48dd0fc
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-08-05 20:15:38 +0900

    Add files via upload

--- project-practice.c
+++ project-practice.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
-int max3(int a, int b, int c)
+int min3(int a, int b, int c)
 {
-	int max = a;
-	if (b > max) max = b;
-	if (c > max) max = c;
+	int min = a;
+	if (b < min) min = b;
+	if (c < min) min = c;
 
-	return max;
+	return min;
 }
 
 int main(void)
@@ -17,8 +17,8 @@ int main(void)
 	printf("整数a：");  scanf("%d", &a);
 	printf("整数b：");  scanf("%d", &b);
 	printf("整数c：");  scanf("%d", &c);
-
-	printf("最大値は%dです。\n", max3(a, b, c));
+	
+	printf("最大値は%dです。\n", min3(a, b, c));
 
 	return 0;
 }
