704df88d7f1b7ab2f7d6a08aef601c5090a83814
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-08-05 20:35:55 +0900

    Add files via upload

--- project-practice.c
+++ project-practice.c
@@ -1,24 +1,25 @@
 #include<stdio.h>
 
-int min3(int a, int b, int c)
+int sqr(int n)
 {
-	int min = a;
-	if (b < min) min = b;
-	if (c < min) min = c;
-
-	return min;
+	return n * n;
 }
+
+int diff(int a, int b)
+{
+	return a > b ? a - b : b - a;
+}
+
 
 int main(void)
 {
-	int a, b, c;
+	int x, y;
 
-	puts("3つの整数を入力せよ。");
-	printf("整数a：");  scanf("%d", &a);
-	printf("整数b：");  scanf("%d", &b);
-	printf("整数c：");  scanf("%d", &c);
-	
-	printf("最大値は%dです。\n", min3(a, b, c));
+	puts("2つの整数を入力");
+	printf("整数x : "); scanf("%d", &x);
+	printf("整数y : "); scanf("%d", &y);
+
+	printf("xの2乗とYの2乗の差は%dです。", diff(sqr(x), sqr(y)));
 
 	return 0;
 }
