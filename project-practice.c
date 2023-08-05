cc5dbd053f5d5250125ae17b1e7a10a8ea7e6cd6
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-08-06 07:49:07 +0900

    Add files via upload

--- project-practice.c
+++ project-practice.c
@@ -1,24 +1,26 @@
 #include<stdio.h>
 
-int sqr(int x)
+double power(double x, int n)
 {
-	return x * x;
-}
+	double tmp = 1.0;
 
-int pow4(int x)
-{
-	return sqr(x) * sqr(x);
+	int i;
+	for (i = 1; i <= n; i++)
+		tmp *= x;
+
+	return tmp;
 }
 
 int main(void)
 {
-	int n1;
+	double a;
+	int b;
 
-	puts("4乗値整数を入力せよ");
-	printf("整数n1 : "); scanf("%d", &n1);
+	printf("aのb乗を求める。\n");
+	printf("実数a : "); scanf("%lf", &a);
+	printf("実数b : "); scanf("%d", &b);
 
-
-	printf("最も大きい値は%dです。\n", pow4(n1));
+	printf("%.2fの%d乗は%.2fです。\n", a, b, power(a, b));
 
 	return 0;
 }
